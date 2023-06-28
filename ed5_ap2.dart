a74df88dfae5e711220f8f3bb7c71bc033fada67
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-06-28 15:05:03 -0300

    AP2 Concluida

--- ed5_ap2.dart
+++ ed5_ap2.dart
@@ -1,1 +1,49 @@
-void main() {}
+import 'dart:math';
+
+void main() {
+  geraNumeros();
+}
+
+void geraNumeros() {
+  final aleatorio = Random();
+  var contador = 0;
+
+  while (contador <= 5) {
+    final numeroAleatorio = aleatorio.nextInt(25) + 1;
+    print("Numero $numeroAleatorio => Letra ${qualALetra(numeroAleatorio)} ");
+    contador++;
+  }
+}
+
+String qualALetra(int numero) {
+  const alfabeto = [
+    "",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  return alfabeto[numero];
+}
