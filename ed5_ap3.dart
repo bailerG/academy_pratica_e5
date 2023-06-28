606bd435db85aa2e8c8cba1dc0bd259be8629d0c
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-06-28 15:36:53 -0300

    AP3 Concluida

--- ed5_ap3.dart
+++ ed5_ap3.dart
@@ -1,1 +1,20 @@
-void main() {}
+import 'dart:math';
+
+void main() {
+  final aleatorio = Random();
+  final numero = aleatorio.nextInt(900) + 100;
+
+  print("A soma dos numeros pares entre 0 e ${numero} é ${somaPares(numero)}}");
+}
+
+int somaPares(int max) {
+  var contador = 0;
+  var soma = 0;
+  while (contador <= max) {
+    if (contador.isEven) {
+      soma += contador;
+    }
+    contador++;
+  }
+  return soma;
+}
