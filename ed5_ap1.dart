80bbe5ca953f8fcb6792e44d2a2d4f033db27e20
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-06-28 14:39:21 -0300

    AP1 Concluida

--- ed5_ap1.dart
+++ ed5_ap1.dart
@@ -1,1 +1,34 @@
-void main() {}
+void main() {
+  final listaImpares = numerosImpares(13);
+  printaLista(listaImpares);
+}
+
+List<int> numerosImpares(int max) {
+  final listaImpares = <int>[];
+  var contador = 0;
+
+  while (contador <= max) {
+    if (contador.isOdd) {
+      listaImpares.add(contador);
+    }
+    contador++;
+  }
+  return listaImpares;
+}
+
+void printaLista(List<int> listaImpares) {
+  for (var numero in listaImpares) {
+    print("Ímpar: $numero");
+  }
+}
+
+// Lógica antes de ver a resolução:
+// void main() {
+//   numerosImpares(10);
+// }
+
+// void numerosImpares(int max) {
+//   for (var i = 0; i <= max; i++) {
+//     i.isOdd ? print("Impar: $i") : null;
+//   }
+// }
