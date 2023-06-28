e732802dc39ed0418916788ecf34c5c1ec040b80
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-06-28 15:53:34 -0300

    AP4 Concluida

--- ed5_ap4.dart
+++ ed5_ap4.dart
@@ -1,1 +1,29 @@
-void main() {}
+void main() {
+  final listaNomes = [
+    "Joao",
+    "Maria",
+    "Pedro",
+    "Maria",
+    "Ana",
+    "Joao",
+    "Maria",
+    "Fernanda",
+    "Carlos",
+    "Maria"
+  ];
+  final nome = 'Guilherme';
+
+  contarNome(listaNomes, nome);
+}
+
+void contarNome(List<String> listaNomes, String nome) {
+  var quantidade = 0;
+  for (var elemento in listaNomes) {
+    if (elemento == nome) {
+      quantidade++;
+    }
+  }
+  quantidade == 0
+      ? print("O nome $nome não está na lista")
+      : print("O nome $nome aparece $quantidade vez(es) na lista");
+}
